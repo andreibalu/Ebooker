f711c78608247f13924f8ba2546461cf5c525286
andreibalu <andrei.baluta@yahoo.com>
2026-03-17 14:55:13 +0200

    Initial Commit

---
+++ EbookerTests/EbookerTests.swift
@@ -0,0 +1,17 @@
+//
+//  EbookerTests.swift
+//  EbookerTests
+//
+//  Created by andreibalu on 17.03.2026.
+//
+
+import Testing
+@testable import Ebooker
+
+struct EbookerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
